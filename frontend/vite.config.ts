9271200d7242902da5080579262f9a5dbf9e4c6b
Jelvys Triana <jelvysc@gmail.com>
2025-03-27 11:22:24 -0400

    Remodelacion del Frontend Inicial

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,19 +1,7 @@
 import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import { fileURLToPath, URL } from 'node:url'
+import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  },
-  server: {
-    port: 3000
-  },
-  build: {
-    target: 'esnext',
-    minify: 'esbuild'
-  }
+  plugins: [react()],
 })
