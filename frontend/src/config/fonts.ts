4ea80b43017f4d2be388fb461031ff5f36ca15eb
Jelvys Triana <jelvysc@gmail.com>
2025-03-30 22:16:30 -0400

    writing navigation

---
+++ frontend/src/config/fonts.ts
@@ -0,0 +1,10 @@
+/**
+ * Configuración de fuentes disponibles en la aplicación
+ * Estas fuentes se utilizan en el archivo tailwind.config.js
+ */
+
+export const fonts = [
+  'inter',
+  'manrope',
+  'sans'
+]; 
