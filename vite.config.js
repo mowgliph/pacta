09cda6d1b5c89ee1cee463d8353e43312e18e02a
Jelvys Triana <jelvysc@gmail.com>
2025-04-13 07:39:50 -0400

    update prisma

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000,
-  },
-  build: {
-    outDir: 'dist',
+    port: 3000
   },
   resolve: {
     alias: {
-      '@': '/src',
-    },
-  },
+      '@': path.resolve(__dirname, './src')
+    }
+  }
 });
