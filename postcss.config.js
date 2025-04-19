5d6bae52584c299c08f960d4cb919ba41b9edf75
Jelvys Triana <jelvysc@gmail.com>
2025-04-19 08:10:12 -0400

    update settings

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,9 @@
 // postcss.config.js
 module.exports = {
-  plugins: [
-    require('postcss-import'),
-    require('tailwindcss'),
-    require('autoprefixer')
-  ]
+  plugins: {
+    'postcss-import': {},
+    'tailwindcss/nesting': {},
+    tailwindcss: {},
+    autoprefixer: {}
+  }
 }
