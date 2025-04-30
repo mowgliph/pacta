bd69aa8a5c385be3234d5657a6664c8eec8caaf8
Jelvys Triana <jelvysc@gmail.com>
2025-05-01 01:14:14 +0200

    update nextron

--- nextron.config.js
+++ nextron.config.js
@@ -1,11 +1,18 @@
 module.exports = {
-    // specify an alternate main src directory, defaults to 'main'
-    mainSrcDir: 'main',
-    // specify an alternate renderer src directory, defaults to 'renderer'
-    rendererSrcDir: 'renderer',
-  
-    // main process' webpack config
-    webpack: (config, env) => {
-      return config
-    },
-  }
+  // specify an alternate main src directory, defaults to 'main'
+  mainSrcDir: "main",
+  // specify an alternate renderer src directory, defaults to 'renderer'
+  rendererSrcDir: "renderer",
+
+  // main process' webpack config
+  webpack: (config, env) => {
+    // Configuración específica para el renderer
+    if (env === "renderer") {
+      config.externals = {
+        ...config.externals,
+        electron: "electron",
+      };
+    }
+    return config;
+  },
+};
